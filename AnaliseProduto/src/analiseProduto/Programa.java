b072cb0df3b25f0fa41c66114cb3ac2fc2eea624
Dayvid Douglas Campelo de Lima <102172970+dayviddouglas@users.noreply.github.com>
2023-09-02 21:52:06 -0300

    Improving code on Programa class.

--- AnaliseProduto/src/analiseProduto/Programa.java
+++ AnaliseProduto/src/analiseProduto/Programa.java
@@ -9,19 +9,20 @@ public class Programa {
 		Scanner input = new Scanner(System.in);
 		
 		
-		System.out.println("Digite o código do produto...");
+		System.out.println("Digite o código do produto... Obs: O número deve ser inteiro maior que 0");
 		int codProdutoUsuario = input.nextInt();
 		input.nextLine();
 		
 		
-		System.out.println("Digite o nome do produto...");
+		System.out.println("Digite o nome do produto... Obs: O produto deve ter pelo menos 5 letras");
 		String nomeProdutoUsuario = input.nextLine();
 		
-		System.out.println("Digite o preço do produto...");
+		System.out.println("Digite o preço do produto... Obs: O preço deve ser em real brasileiro.");
 		String precoProdutoUsuario = input.nextLine();
 		
+		 
+			   Produto p = new Produto(codProdutoUsuario,nomeProdutoUsuario,precoProdutoUsuario);
 		   
-			Produto p = new Produto(codProdutoUsuario,nomeProdutoUsuario,precoProdutoUsuario);
 		
 		
         
